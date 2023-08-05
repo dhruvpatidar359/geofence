b3085a7a8db2ec6344aa570ae7432f3c2d547a57
dhruvpatidar359 <dhruvpatidar35@gmail.com>
2023-08-05 22:36:49 +0530

    added login in progress

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import '../constants/colors.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -8,8 +11,290 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  Widget radioButton(bool isSelected) => Container(
+        width: 16.0,
+        height: 16.0,
+        padding: EdgeInsets.all(2.0),
+        decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            border: Border.all(width: 2.0, color: Colors.black)),
+        child: isSelected
+            ? Container(
+                width: double.infinity,
+                height: double.infinity,
+                decoration:
+                    BoxDecoration(shape: BoxShape.circle, color: Colors.black),
+              )
+            : Container(),
+      );
+
+  Widget horizontalLine() => Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16.0),
+        child: Container(
+          width: 120,
+          height: 1.0,
+          color: Colors.black26.withOpacity(.2),
+        ),
+      );
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      resizeToAvoidBottomInset: true,
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: new AssetImage('assets/images/back.jpg'),
+            fit: BoxFit.fill,
+          ),
+        ),
+        child: Stack(
+          fit: StackFit.expand,
+          children: <Widget>[
+            SingleChildScrollView(
+              child: Padding(
+                padding: EdgeInsets.only(left: 28.0, right: 28.0, top: 60.0),
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: <Widget>[
+                        Image.asset(
+                          "assets/images/logo.png",
+                          width: 220,
+                          height: 220,
+                        ),
+                        SizedBox(
+                          width: 40,
+                        ),
+                        Expanded(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisSize: MainAxisSize.min,
+                            children: <Widget>[
+                              Text("GeoFlix",
+                                  style: TextStyle(
+                                      fontFamily: "Poppins-Bold",
+                                      color: appbarcolor,
+                                      fontSize: 90,
+                                      letterSpacing: .6,
+                                      fontWeight: FontWeight.bold)),
+                              Text("Geo-Attendance and HR Management System",
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                      fontFamily: "Poppins-Bold",
+                                      color: Colors.black54,
+                                      fontSize: 25,
+                                      letterSpacing: 0.2,
+                                      fontWeight: FontWeight.bold)),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 90,
+                    ),
+                    formCard(),
+                    SizedBox(height: 40),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        InkWell(
+                          child: Container(
+                            width: 330,
+                            height: 100,
+                            decoration: BoxDecoration(
+                                gradient: LinearGradient(colors: [
+                                  splashScreenColorBottom,
+                                  Color(0xFF6078ea)
+                                ]),
+                                borderRadius: BorderRadius.circular(6.0),
+                                boxShadow: [
+                                  BoxShadow(
+                                      color: Color(0xFF6078ea).withOpacity(.3),
+                                      offset: Offset(0.0, 8.0),
+                                      blurRadius: 8.0)
+                                ]),
+                            child: Material(
+                              color: Colors.transparent,
+                              child: InkWell(
+                                onTap: () {}, // validate and submit
+                                child: Center(
+                                  child: Text("LOGIN",
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontFamily: "Poppins-Bold",
+                                          fontSize: 18,
+                                          letterSpacing: 1.0)),
+                                ),
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        horizontalLine(),
+                        Text("Other Options",
+                            style: TextStyle(
+                                fontSize: 16.0, fontFamily: "Poppins-Medium")),
+                        horizontalLine()
+                      ],
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          "Don't have Login Details? ",
+                          style: TextStyle(fontFamily: "Poppins-Medium"),
+                        ),
+                        InkWell(
+                          onTap: () {},
+                          child: Text("Contact Admin",
+                              style: TextStyle(
+                                  color: splashScreenColorTop,
+                                  fontFamily: "Poppins-Bold")),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget formCard() {
+    return new Container(
+      width: double.infinity,
+      height: 260,
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(8.0),
+          boxShadow: [
+            BoxShadow(
+                color: Colors.black12,
+                offset: Offset(0.0, 15.0),
+                blurRadius: 15.0),
+            BoxShadow(
+                color: Colors.black12,
+                offset: Offset(0.0, -10.0),
+                blurRadius: 10.0),
+          ]),
+      child: Padding(
+        padding: EdgeInsets.only(left: 16.0, right: 16.0, top: 16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text("Login",
+                  style: TextStyle(
+                      fontSize: 45,
+                      fontFamily: "Poppins-Bold",
+                      letterSpacing: .6)),
+              SizedBox(
+                height: 30,
+              ),
+              Container(
+                height: 60,
+                child: TextFormField(
+                  decoration: InputDecoration(
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: dashBoardColor),
+                      ),
+                      icon: Icon(
+                        Icons.person,
+                        color: dashBoardColor,
+                      ),
+                      hintText: "Employee ID",
+                      hintStyle: TextStyle(color: Colors.grey, fontSize: 15.0)),
+                  validator: (value) => value == null || value.isEmpty
+                      ? 'Username can\'t be empty'
+                      : null,
+                  onSaved: (value) => _username = value?.trim(),
+                ),
+              ),
+              Container(
+                height: 60,
+                child: TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: dashBoardColor),
+                      ),
+                      icon: Icon(
+                        Icons.lock,
+                        color: dashBoardColor,
+                      ),
+                      hintText: "Password",
+                      hintStyle: TextStyle(color: Colors.grey, fontSize: 15.0)),
+                  validator: (value) => value == null || value.isEmpty
+                      ? 'Password can\'t be empty'
+                      : null,
+                  onChanged: (value) => _password = value,
+                ),
+              ),
+              Text(
+                _errorMessage,
+                style: TextStyle(color: Colors.red),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  TextButton(
+                    style: ButtonStyle(
+                      padding: MaterialStateProperty.resolveWith(
+                        (states) => EdgeInsets.symmetric(horizontal: 16.0),
+                      ),
+                      shape: MaterialStateProperty.resolveWith(
+                        (states) => const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(2.0)),
+                        ),
+                      ),
+                      backgroundColor: MaterialStateProperty.resolveWith(
+                        (states) => Colors.blue,
+                      ),
+                    ),
+                    onPressed: () => _formKey.currentState?.reset(),
+                    child: Text(
+                      "Reset",
+                      style: TextStyle(
+                          color: dashBoardColor,
+                          fontFamily: "Poppins-Medium",
+                          fontSize: 28),
+                    ),
+                  ),
+                  Text(
+                    "Forgot Password?",
+                    style: TextStyle(
+                        color: dashBoardColor,
+                        fontFamily: "Poppins-Medium",
+                        fontSize: 28),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
