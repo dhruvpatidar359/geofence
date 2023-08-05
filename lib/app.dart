5e454e66407cb9dc6eea0f037b9d3cd47c647657
dhruvpatidar359 <dhruvpatidar35@gmail.com>
2023-08-05 22:17:46 +0530

    added tree

---
+++ lib/app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:geofence/pages/login.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LoginScreen();
+  }
+}
